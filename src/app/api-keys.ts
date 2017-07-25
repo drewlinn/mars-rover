526b3e7b65141ffc7e9cc483398ca48626006fa0
Andrew <expandrew@gmail.com>
2017-07-24 19:47:02 -0700

    finish lesson on Web APIs and Authentication with Angular/Implementing Web APIs.

---
+++ src/app/api-keys.ts
@@ -0,0 +1,1 @@
+export const marsRoverKey = "HbjdNFxT21YvBj1l66qJ5fW1g4rzYhjZtC7q3BoF"
---
+++ src/app/rover-form/rover-form.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { MarsRoverAPIPhotos } from '../mars-rover-api-photos.service';
+
+@Component({
+  selector: 'app-rover-form',
+  templateUrl: './rover-form.component.html',
+  styleUrls: [ './rover-form.component.css' ],
+  providers: [ MarsRoverAPIPhotos ]
+})
+
+export class RoverFormComponent {
+  photos: any[]=null;
+  noPhotos: boolean=false;
+  constructor(private marsRoverPhotos: MarsRoverAPIPhotos) { }
+
+  getRoverImages(date: string, camera: string) {
+    this.noPhotos = false;
+    this.marsRoverPhotos.getByDateAndCamera(date, camera).subscribe(response =>{
+      if(response.json().photos.length > 0)
+      {
+        this.photos = response.json();
+      }
+      else {
+        this.noPhotos = true;
+      }
+    });
+  }
+}
